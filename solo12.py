Fix the rotation function to express IMU data in the base frame

solo12.py
@@ -15,12 +15,12 @@ class Solo12(RobotHAL):
         self.motorKt = np.array(self.nb_motors * [0.025, ])  # Nm/A
         self.motorSign = np.array([+1, -1, -1, -1, +1, +1,
                                    +1, -1, -1, -1, +1, +1])
-        self.maximumCurrent = 3.0  # A
+        self.maximumCurrent = 5.0  # A
         # To get this offsets, run the calibration with self.encoderOffsets at 0,
         # then manualy move the robot in zero config, and paste the position here (note the negative sign!)
         self.encoderOffsets = - np.array([1.940310, -2.658198, -2.893262, -1.918342,
                                           1.501880, -0.694378, 2.974716, -0.415520, -0.326077, 0.444250, 2.433491, -0.384136])
         # self.encoderOffsets *= 0.
         # 180 degree roll
-        self.rotateImuVectors = lambda x: [x[0], -x[1], -x[2]]
+        self.rotateImuVectors = lambda x: [-x[0], -x[1], x[2]]
         self.rotateImuOrientation = lambda q: [q[3], -q[2], -q[1], q[0]]