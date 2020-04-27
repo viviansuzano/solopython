e410e6a9104c15f1e60d7a03324f289796a45b54
maximekli <maxime.klimecky@outlook.com>
2020-04-27 14:36:30 +0200

    Added test bench HAL + calibration

---
+++ test_bench.py
@@ -0,0 +1,26 @@
+import numpy as np
+from utils.abstractRobotHal import RobotHAL
+
+class TestBench(RobotHAL):
+    ''' Define the hardware interface to the test bench'''
+
+    def __init__(self, interfaceName="", dt=0.001):
+        RobotHAL.__init__(self, interfaceName, dt)
+
+    def InitRobotSpecificParameters(self):
+        ''' Definition of the test bench paramters '''
+        self.nb_motors = 2
+        self.motorToUrdf = [0, 1]
+        self.gearRatio = np.array(self.nb_motors * [1., ])  # gearbox ratio
+        self.motorKt = np.array(self.nb_motors * [0.025, ])  # Nm/A
+        self.motorSign = np.array([+1, -1])
+        
+        self.maximumCurrent = 3.0  # A
+        # To get this offsets, run the calibration with self.encoderOffsets at 0,
+        # then manualy move the robot in zero config, and paste the position here (note the negative sign!)
+        self.encoderOffsets = - \
+            np.array([1.02373588, 0.24259591]) # Zero arrows up
+        #self.encoderOffsets *= 0.
+        self.rotateImuVectors = lambda x: [x[0], x[1], x[2]] # No IMU for the test benches
+        self.rotateImuOrientation = lambda q: [q[0], q[1], q[2], q[3]] # No IMU for the test benches
+        
