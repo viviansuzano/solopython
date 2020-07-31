529106724120f656a0f2a02b9c5d1a1cbe8de75f
maximekli <maxime.klimecky@outlook.com>
2020-07-31 16:48:17 +0200

    Update solo8 encoder offsets

--- solo8.py
+++ solo8.py
@@ -14,13 +14,13 @@ class Solo8(RobotHAL):
         self.gearRatio = np.array(self.nb_motors * [9., ])  # gearbox ratio
         self.motorKt = np.array(self.nb_motors * [0.025, ])  # Nm/A
         self.motorSign = np.array([-1, -1, +1, +1, -1, -1, +1, +1])
-        
         self.maximumCurrent = 3.0  # A
         # To get this offsets, run the calibration with self.encoderOffsets at 0,
         # then manualy move the robot in zero config, and paste the position here (note the negative sign!)
-        self.encoderOffsets = - \
-            np.array([1.660367, -2.610352,  2.866129,  1.009784,
-                      0.620769, -1.710268,  2.117314,  -4.056512])
+        self.encoderOffsets = - np.array([2.1918368339538574, 3.7512590885162354, -3.4104650020599365, 1.0086714029312134, 3.788919448852539, 0.9612823724746704, 2.1028778553009033, 2.1483843326568604])
+        
         #self.encoderOffsets *= 0.
         self.rotateImuVectors = lambda x: [x[1], x[0], -x[2]]
         self.rotateImuOrientation = lambda q: [-q[1], -q[0], q[2], -q[3]]
+
+
