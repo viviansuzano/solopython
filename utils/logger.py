e7e7e6147963572d7517126ea419af9701decc23
odri <odri@furano.laas.fr>
2020-11-12 17:12:03 +0100

    Add logging of estimator quantities, timestamps, invkin

--- utils/logger.py
+++ utils/logger.py
@@ -1,7 +1,7 @@
 '''This class will log 1d array in Nd matrix from device and qualisys object'''
 import numpy as np
 from datetime import datetime as datetime
-
+from time import clock
 
 class Logger():
     def __init__(self, device, qualisys=None, logSize=60e3, ringBuffer=False):
@@ -20,6 +20,9 @@ class Logger():
         self.baseAngularVelocity = np.zeros([logSize, 3])
         self.baseLinearAcceleration = np.zeros([logSize, 3])
         self.baseAccelerometer = np.zeros([logSize, 3])
+        self.baseLinearVelocity = np.zeros([logSize, 3])
+        self.appliedForce = np.zeros([logSize, 3])
+        self.dummyPos = np.zeros([logSize, 3])
 
         # Motion capture:
         self.mocapPosition = np.zeros([logSize, 3])
@@ -35,6 +38,17 @@ class Logger():
         self.referenceVelocity = np.zeros([logSize, 6])
         self.logXFMPC = np.zeros([logSize, 24])
 
+        self.log_HP_lin_vel = np.zeros([logSize, 3])
+        self.log_IMU_lin_acc = np.zeros([logSize, 3])
+        self.log_LP_lin_vel = np.zeros([logSize, 3])
+        self.log_FK_lin_vel = np.zeros([logSize, 3])
+        self.log_filt_lin_vel = np.zeros([logSize, 3])
+        self.q_filt = np.zeros([logSize, 19])
+        self.v_filt = np.zeros([logSize, 18])
+
+        # Timestamps
+        self.tstamps = np.zeros(logSize)
+
     def sample(self, device, qualisys=None, estimator=None):
         if (self.i >= self.logSize):
             if self.ringBuffer:
@@ -49,6 +63,12 @@ class Logger():
         self.baseLinearAcceleration[self.i] = device.baseLinearAcceleration
         self.baseAccelerometer[self.i] = device.baseAccelerometer
         self.torquesFromCurrentMeasurment[self.i] = device.torquesFromCurrentMeasurment
+
+        # REMOVE WHEN ON REAL ROBOT
+        """self.baseLinearVelocity[self.i] = device.b_baseVel
+        self.appliedForce[self.i] = device.pyb_sim.applied_force
+        self.dummyPos[self.i] = device.dummyPos"""
+
         # Logging from qualisys
         if qualisys is not None:
             self.mocapPosition[self.i] = qualisys.getPosition()
@@ -63,6 +83,18 @@ class Logger():
             self.contactStatus[self.i] = estimator.contactStatus
             self.referenceVelocity[self.i] = estimator.v_ref.ravel()
             self.logXFMPC[self.i] = estimator.x_f_mpc.ravel()
+
+            self.log_HP_lin_vel[self.i] = estimator.HP_lin_vel[:]
+            self.log_IMU_lin_acc[self.i] = estimator.IMU_lin_acc[:]
+            self.log_LP_lin_vel[self.i] = estimator.LP_lin_vel[:]
+            self.log_FK_lin_vel[self.i] = estimator.FK_lin_vel[:]
+            self.log_filt_lin_vel[self.i] = estimator.filt_lin_vel[:]
+            self.q_filt[self.i] = estimator.q_filt[:, 0]
+            self.v_filt[self.i] = estimator.v_filt[:, 0]
+
+        # Logging timestamp
+        self.tstamps[self.i] = clock()
+
         self.i += 1
 
     def saveAll(self, fileName="data"):
@@ -74,6 +106,8 @@ class Logger():
                  torquesFromCurrentMeasurment=self.torquesFromCurrentMeasurment,
                  baseOrientation=self.baseOrientation,
                  baseAngularVelocity=self.baseAngularVelocity,
+                 baseLinearVelocity=self.baseLinearVelocity,
+                 appliedForce=self.appliedForce,
                  baseLinearAcceleration=self.baseLinearAcceleration,
                  baseAccelerometer=self.baseAccelerometer,
                  mocapPosition=self.mocapPosition,
@@ -85,4 +119,6 @@ class Logger():
                  estimatorVelocity=self.estimatorVelocity,
                  contactStatus=self.contactStatus,
                  referenceVelocity=self.referenceVelocity,
-                 logXFMPC=self.logXFMPC)
+                 logXFMPC=self.logXFMPC,
+                 tstamps=self.tstamps,
+                 dummyPos=self.dummyPos)
